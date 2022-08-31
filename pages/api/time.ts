d42aaa24f98f4f83b3c11696d3dc181037398a8e
VanteyGonnaDie <bahya.d97@gmail.com>
2022-08-31 23:10:58 +0300

    testing child_process

--- pages/api/time.ts
+++ pages/api/time.ts
@@ -1,15 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { stringify } from 'querystring'
+import { exec } from 'child_process';
 
 type Data = {
   time: string
   request: any
+  status: any
 }
 
-export default function handler(
+
+export default function  handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ time: `${(Date.now())}` , request: req.headers})
+  // exec('echo "test"', (error:any, stdout:any, stderr:any) => {
+    exec('cd', (error:any, stdout:any, stderr:any) => {
+    let status
+    if (error) {
+      console.error(`error: ${error.message}`);
+      status = error
+      return;
+    }
+    
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      status = stderr
+      return;
+    }
+    status = stdout
+    console.log(`stdout:\n${stdout}`);
+    res.status(200).json({ time: `${new Date(Date.now())}` , request: req.headers, status: status })
+  });
 }
